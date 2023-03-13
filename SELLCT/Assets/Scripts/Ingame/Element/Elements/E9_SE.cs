4e9032c43632395f28077f7167eef04a6d68a647
GTM106 <raito125dragon7@gmail.com>
2023-03-13 18:09:41 +0900

    E9の仮組みを実装しました。
    Sound系は未実装のため、仮組みだけの実装です。Spriteは実装済みです。

---
+++ SELLCT/Assets/Scripts/Ingame/Element/Elements/E9_SE.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E9_SE : Card
+{
+    [SerializeField] CardParameter _parameter = default!;
+    [SerializeField] MoneyPossessedController _controller = default!;
+    [SerializeField] Sprite _baseSprite = default!;
+    [SerializeField] Sprite _number = default!;
+    [SerializeField] Sprite _chineseCharacters = default!;
+    [SerializeField] Sprite _hiragana = default!;
+    [SerializeField] Sprite _katakana = default!;
+    [SerializeField] HandMediator _handMediator = default!;
+
+    readonly List<Sprite> result = new();
+
+    public override bool IsDisposedOfAfterSell => _parameter.IsDisposedOfAfterSell();
+    public override int Rarity => _parameter.Rarity();
+    public override IReadOnlyList<Sprite> CardSprite
+    {
+        get
+        {
+            //èâä˙âª
+            if (result.Count == 0)
+            {
+                result.Add(_baseSprite);
+                result.Add(_number);
+                result.Add(_chineseCharacters);
+                result.Add(_hiragana);
+                result.Add(_katakana);
+            }
+
+            return result;
+        }
+    }
+
+    public override void Buy()
+    {
+        //TODO:SEÇÃçƒê∂
+
+        _controller.DecreaseMoney(_parameter.GetMoney());
+    }
+
+    public override void Passive()
+    {
+        // DoNothing
+    }
+
+    public override void Sell()
+    {
+        StopSE();
+
+        _controller.IncreaseMoney(_parameter.GetMoney());
+    }
+
+    private void StopSE()
+    {
+        if (_handMediator.ContainsCard(this)) return;
+
+        //TODO:SEÇÃí‚é~
+    }
+}
